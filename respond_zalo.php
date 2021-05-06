822e0329f7b188a2ba747067bc46a914ef73f0a1
NguyenThanhLong <long.nt184287@sis.hust.edu.vn>
2021-05-06 10:00:44 +0700

    add test case??

--- respond_zalo.php
+++ respond_zalo.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+echo "Hi!";
 
 $callbackPageUrl = "https://www.callbackPage.com";
 $linkOAGrantPermission2App = $helper->getLoginUrlByPage($callbackPageUrl); // This is url for admin OA grant permission to app
 
+echo "Also hi!";
 ?>
