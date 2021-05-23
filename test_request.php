da611eaad8d19ccd51906706b8618c8cf39a97e6
NguyenThanhLong <long.nt184287@sis.hust.edu.vn>
2021-05-23 17:43:26 +0700

    edit log2

--- test_request.php
+++ test_request.php
@@ -32,7 +32,6 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 
 // Execute the POST request
 $result = curl_exec($ch);
-sleep(1);
 $result = curl_exec($ch);
 
 // Close cURL resource
